c63f8a0cff03a1097453d33e6a479994dda1f5f4
gandhi <gandhisaputra13@gmail.com>
2020-07-09 16:56:57 +0700

    model jadwal rundown

---
+++ app/InsRundown.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class InsRundown extends Model
+{
+    //
+    use SoftDeletes;
+    protected $table = "instruktur_rundown";
+
+    // relasi belong to instruktur
+    public function instruktur_r(){
+        return $this->belongsTo('App\InstrukturModel','id_instruktur');
+    }
+
+    // relasi belongs to modul
+    public function jadwal_rundown_r(){
+        return $this->belongsTo('App\JadwalRundown','id_rundown');
+    }
+}
